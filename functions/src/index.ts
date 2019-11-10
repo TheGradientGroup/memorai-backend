44e43959427b59595ec7fdd3e188211c97169254
Willie Chalmers III <willieechalmers@gmail.com>
2019-11-10 07:02:36 -0600

    Update Cloud Functions config for new endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,14 @@
 import * as functions from 'firebase-functions';
 import { handleCards, handleDecks } from './cards';
+import { handleSessions } from './sessions';
 // import { handlePrediction } from './predict';
 // import { handleTrain } from './train';
 
 export const posts = functions.https.onRequest(handleCards);
 
 export const decks = functions.https.onRequest(handleDecks);
+
+export const sessions = functions.https.onRequest(handleSessions);
 
 // export const predict = functions.https.onRequest(handlePrediction);
 
