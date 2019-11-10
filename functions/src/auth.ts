8dfb18f31cc8a529d97d7bcad884234bde738159
Willie Chalmers III <willieechalmers@gmail.com>
2019-11-09 18:03:04 -0600

    Convert promise-based code to try/catch

--- functions/src/auth.ts
+++ functions/src/auth.ts
@@ -1,5 +1,6 @@
 import * as admin from 'firebase-admin';
-const app = admin.initializeApp();
+
+admin.initializeApp();
 
 /**
  * Checks if an auth token is valid.
@@ -7,24 +8,17 @@ const app = admin.initializeApp();
  * This checks if the given auth token is a valid format and is expired using
  * Firebase Auth. 
  * 
- * @param {String} token A token to check 
+ * @param {string} token A token to check 
  * 
- * @return {Boolean} True if the auth token is valid, false otherwise. 
+ * @return {Promise<string>} Promise that resolve to the user ID from the
+ * given token. 
  */
-async function verifyToken(token: string): Promise<String> {
-
-  return admin.auth().verifyIdToken(token)
-    .then(function(decodedToken) {
-      let uid = decodedToken.uid;
-      return uid;
-
-    })
-    .catch(function(error) {
-      throw error;
-    });
-
-  
-
-
-
+export async function verifyToken(token: string): Promise<String> {
+  try {
+    const decodedToken = await admin.auth().verifyIdToken(token);
+    const uid = decodedToken.uid;
+    return uid;
+  } catch (e) {
+    throw e;
+  }
 }
