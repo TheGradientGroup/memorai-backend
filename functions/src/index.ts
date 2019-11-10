8cc986b72fac6048c3482d8fbc926ae120314ced
Willie Chalmers III <willieechalmers@gmail.com>
2019-11-10 03:25:09 -0600

    Add posts and decks endpoints to functions config

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,12 @@
 import * as functions from 'firebase-functions';
-import { handlePrediction } from './predict';
-import { handleCards } from './cards';
-import { handleTrain } from './train';
+import { handleCards, handleDecks } from './cards';
+// import { handlePrediction } from './predict';
+// import { handleTrain } from './train';
 
 export const posts = functions.https.onRequest(handleCards);
 
-export const predict = functions.https.onRequest(handlePrediction);
+export const decks = functions.https.onRequest(handleDecks);
 
-export const train = functions.https.onRequest(handleTrain);
+// export const predict = functions.https.onRequest(handlePrediction);
+
+// export const train = functions.https.onRequest(handleTrain);
